194d787777fc4ccf5aff53ff59cd89179f8f4506
Tim <tim@higheraid.com>
2013-03-18 16:03:40 -0400

    Finish ch3 advanced settings

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
+	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
 	describe "Home page" do
 
 		it "should have the h1 'Sample App'" do
@@ -13,7 +15,7 @@ describe "Static Pages" do
 		it "should have the right title" do
 			visit '/static_pages/home'
 			page.should have_selector('title', 
-				:text => "Ruby on Rails Tutorial Sample App | Home")
+				:text => "#{base_title} | Home")
 		end
 	end
 
@@ -28,7 +30,7 @@ describe "Static Pages" do
 		it "should have the right title" do
 			visit '/static_pages/help'
 			page.should have_selector('title', 
-				:text => "Ruby on Rails Tutorial Sample App | Help")
+				:text => "#{base_title} | Help")
 		end
 	end
 
@@ -43,7 +45,22 @@ describe "Static Pages" do
 		it "should have the right title" do
 			visit '/static_pages/about'
 			page.should have_selector('title', 
-				:text => "Ruby on Rails Tutorial Sample App | About Us")
+				:text => "#{base_title} | About Us")
+		end
+	end
+
+	describe "Contact page" do
+
+		it "should have the h1 'Contact Us'" do
+			visit '/static_pages/contact'
+			page.should have_selector('h1', 
+				:text =>'Contact Us')
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/contact'
+			page.should have_selector('title', 
+				:text => "#{base_title} | Contact Us")
 		end
 	end
 
